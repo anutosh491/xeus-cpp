969a90fb37922cd12a5183be89789bc0bf7d5a44
anutosh491 <andersonbhat491@gmail.com>
2024-11-05 12:44:26 +0530

    address reviews

--- src/xinterpreter.cpp
+++ src/xinterpreter.cpp
@@ -42,10 +42,9 @@ void* createInterpreter(const Args &ExtraArgs = {}) {
       std::string resource_dir = Cpp::DetectResourceDir();
       if (resource_dir.empty())
       {
-        std::cerr << "Failed to detect the resource-dir\n";
+          std::cerr << "Failed to detect the resource-dir\n";
       }
-      ClangArgs.push_back("-resource-dir");
-      ClangArgs.push_back(resource_dir.c_str());
+      ClangArgs.push_back("-resource-dir " + resource_dir);
   }
   std::vector<std::string> CxxSystemIncludes;
   Cpp::DetectSystemCompilerIncludePaths(CxxSystemIncludes);
