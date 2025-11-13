ffe5cf213afdc6323876326879b0153eeb840486
Anutosh Bhat <andersonbhat491@gmail.com>
2025-11-13 10:36:54 +0530

    Update test_interpreter.cpp

--- test/test_interpreter.cpp
+++ test/test_interpreter.cpp
@@ -212,6 +212,7 @@ TEST_SUITE("Test Stream Redirection")
         REQUIRE(1 == 1);
         std::vector<const char*> Args = {"-v"};
         xcpp::interpreter interpreter((int)Args.size(), Args.data());
+        interpreter.restore_output();
 
         xeus::execute_request_config config;
         config.silent = false;
